f73b52b0fccaad8a537e2290059d22f853dcfcec
Tuanemendes <tuane.belmiro@gmail.com>
2023-05-17 11:18:26 -0300

    aula1

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,7 +1,7 @@
-import Image from 'next/image'
-
 export default function Home() {
   return (
-    <h1>Hello Word</h1>
+    <div className="h-screen bg-zinc-950 p-6 text-zinc-50">
+      <h1 className="text-4xl font-bold">Sua cápsula do tempo</h1>
+    </div>
   )
 }
