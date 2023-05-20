7a98deccec2a4401b66ebafcaf6ed5ade7cfc1c2
Tuanemendes <tuane.belmiro@gmail.com>
2023-05-20 16:54:30 -0300

    aula 3 server

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,15 +1,27 @@
+import 'dotenv/config'
 import fastify from 'fastify'
 import cors from '@fastify/cors'
+import jwt from '@fastify/jwt'
+
 import { memoriesRoutes } from './routes/memories';
+import { authRoutes } from './routes/auth';
 const app = fastify();
 
-app.register(memoriesRoutes)
+
 app.register(cors,{
     origin: 'true', // Access-Control-Allow-Origin: true
 })
+app.register(jwt,{
+    secret: 'spacetime',
+})
+
+app.register(authRoutes)
+app.register(memoriesRoutes)
+
 
 app.listen({
     port: 3333,
+    host:'0.0.0.0',
 }).then(()=>{
     console.log('HTTP Server is running on http://localhost:3333');
 })
