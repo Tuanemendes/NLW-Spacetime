d082772d9e2d0ae34bd20077690f4cb446ccd7c3
Tuanemendes <tuane.belmiro@gmail.com>
2023-05-20 18:07:09 -0300

    aula 3 mobile

--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -1,5 +1,10 @@
 import { StatusBar } from 'expo-status-bar'
+import { makeRedirectUri, useAuthRequest } from 'expo-auth-session'
+import { useEffect } from 'react'
 import { ImageBackground, Text, TouchableOpacity, View } from 'react-native'
+import { styled } from 'nativewind'
+import * as SecureStore from 'expo-secure-store'
+import { useRouter } from 'expo-router'
 
 import {
   useFonts,
@@ -9,19 +14,66 @@ import {
 
 import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree'
 
-import blurBg from './src/assets/bg-blur.png'
-import Stripes from './src/assets/stripes.svg'
-import NLWLogo from './src/assets/nlw-spacetime.svg'
-import { styled } from 'nativewind'
+import blurBg from '../src/assets/bg-blur.png'
+import Stripes from '../src/assets/stripes.svg'
+import NLWLogo from '../src/assets/nlw-spacetime.svg'
+import { api } from '../src/lib/api'
+
+// Endpoint
+const discovery = {
+  authorizationEndpoint: 'https://github.com/login/oauth/authorize',
+  tokenEndpoint: 'https://github.com/login/oauth/access_token',
+  revocationEndpoint:
+    'https://github.com/settings/connections/applications/<b6ba1c4c4455e431cd46>',
+}
 
 const StyledStripes = styled(Stripes)
 
 export default function App() {
+  const router = useRouter()
+
   const [hasLoadedFonts] = useFonts({
     Roboto_400Regular,
     Roboto_700Bold,
     BaiJamjuree_700Bold,
   })
+
+  const [, response, signInGitHub] = useAuthRequest(
+    {
+      clientId: 'b6ba1c4c4455e431cd46',
+      scopes: ['identity'],
+      redirectUri: makeRedirectUri({
+        scheme: 'nlwspacetime',
+      }),
+    },
+    discovery,
+  )
+
+  async function handleGitHubOAuthCode(code: string) {
+    const response = await api.post('/register', {
+      code,
+    })
+    const { token } = response.data
+
+    await SecureStore.setItemAsync('token', token)
+
+    router.push('/memories')
+  }
+
+  useEffect(() => {
+    // para pegar a url correta para o redirectUri
+    // console.log(
+    //   makeRedirectUri({
+    //     scheme: 'nlwspacetime',
+    //   }),
+    // )
+
+    if (response?.type === 'success') {
+      const { code } = response.params
+
+      handleGitHubOAuthCode(code)
+    }
+  }, [response])
 
   if (!hasLoadedFonts) {
     return null
@@ -49,6 +101,7 @@ export default function App() {
         <TouchableOpacity
           activeOpacity={0.8}
           className="rounded-full bg-green-500 px-5 py-2"
+          onPress={() => signInGitHub()}
         >
           <Text className="font-alternative text-sm uppercase text-black">
             Cadastrar Lembraça
---
+++ mobile/src/lib/api.ts
@@ -0,0 +1,4 @@
+import axios from 'axios'
+export const api = axios.create({
+  baseURL: 'http://192.168.18.7:3333',
+})
